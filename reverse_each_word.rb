5586e6906db32db18873705fcea3f1ef8c377905
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-04-29 01:25:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(sentence)
-  reverse_sentence = sentence.split.collect {|new_word| new_word.reverse}
+  reverse_sentence = sentence.split.collect { |new_word| new_word.reverse }
 end
+  reverse_sentece.join(" ")
 end
